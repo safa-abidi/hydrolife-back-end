dadbd4b0b91a0125b990aa7248f0c4efb228ecd8
Safa Abidi <61377020+safa-abidi@users.noreply.github.com>
2021-03-27 17:20:04 +0100

    fixed photo

--- src/main/java/tn/hydrolife/hydrolifeBackEnd/controllers/PhotoController.java
+++ src/main/java/tn/hydrolife/hydrolifeBackEnd/controllers/PhotoController.java
@@ -14,7 +14,6 @@ import tn.hydrolife.hydrolifeBackEnd.entities.Centre;
 import tn.hydrolife.hydrolifeBackEnd.entities.Photo;
 import tn.hydrolife.hydrolifeBackEnd.repositories.PhotoRepository;
 import tn.hydrolife.hydrolifeBackEnd.services.CentreService;
-import tn.hydrolife.hydrolifeBackEnd.services.PhotoService;
 import javax.servlet.ServletContext;
 import org.apache.commons.io.FilenameUtils;
 
@@ -32,44 +31,48 @@ public class PhotoController {
     @Autowired
     ServletContext context;
 
-    @PostMapping("/add")
-    public ResponseEntity<Photo> addPhoto(@RequestParam("file")MultipartFile file, @RequestParam("photo") String photo) throws JsonParseException, JsonMappingException, Exception {
-
-        Photo thePhoto = new ObjectMapper().readValue(photo, Photo.class);
-        boolean isExist = new File(context.getRealPath("/Images/")).exists();
-
-        if(!isExist){
-            new File(context.getRealPath("/Images/")).mkdir();
+    @PostMapping(value="/add")
+    public ResponseEntity<Photo> createDocument (@RequestParam("file") MultipartFile file,
+                                                 @RequestParam("photo") String photo) throws JsonParseException , JsonMappingException , Exception
+    {
+        Photo photoo = new ObjectMapper().readValue(photo,Photo.class);
+        boolean isExit = new File(context.getRealPath("/Images/")).exists();
+        if (!isExit)
+        {
+            new File (context.getRealPath("webapp/Images/")).mkdir();
         }
+        String filename = file.getOriginalFilename();
+        String newFileName = FilenameUtils.getBaseName(filename)+"."+FilenameUtils.getExtension(filename);
+        File serverFile = new File (context.getRealPath("/Images/"+File.separator+newFileName));
+        try
+        {
+            FileUtils.writeByteArrayToFile(serverFile,file.getBytes());
 
-        String fileName = file.getOriginalFilename();
-        String newFileName = FilenameUtils.getBaseName(fileName)+"."+FilenameUtils.getExtension(fileName);
-        File serverFile = new File(context.getRealPath("/Images/"+File.separator+newFileName));
-        try{
-            FileUtils.writeByteArrayToFile(serverFile, file.getBytes());
-        }catch(Exception e){
+        }catch(Exception e) {
             e.printStackTrace();
         }
 
-        thePhoto.setFileName(newFileName);
+        photoo.setFileName(newFileName);
 
         //get current logged centre
         Optional<Centre> currentCentre = centreService.getCurrentCentre();
         //get his id
         Long currentCentreId = currentCentre.get().getId();
+
         //set idCentre in the photo
-        thePhoto.setIdCentre(currentCentreId);
+        photoo.setIdCentre(currentCentreId);
 
         //l'ajouter à ce centre
-        currentCentre.get().getPhotos().add(thePhoto);
+        currentCentre.get().getPhotos().add(photoo);
 
-        Photo ph = photoRepository.save(thePhoto);
-        if(ph != null){
-            return new ResponseEntity<>(thePhoto, HttpStatus.OK);
-        }else{
+        Photo savePhoto = photoRepository.save(photoo);
+        if (savePhoto!= null)
+        {
+            return new ResponseEntity<>(savePhoto,HttpStatus.OK);
+        }
+        else
+        {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
-
-
 }
